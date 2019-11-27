76b099dcb4538e28da8896b77ffeabada4e503f9
Pooja Chaurasiya <chaurasiyapooja05@gmail.com>
2019-11-27 07:36:28 +0530

    Amdements Completed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast =(latitude,longitude,callback)=>{
         }else if(body.error){
             callback('Unable to find the location. Try another search.',undefined)
         }else{
-            callback(undefined,(body.currently.summary+' It is currently '+body.currently.temperature+' degree out. There is a '+body.currently.precipProbability+' Chance of rain'))
+            callback(undefined,(' The Temperature is high at '+body.daily.data[0].temperatureHigh+'. The Low is at '+body.daily.data[0].temperatureLow+'. '+body.daily.data[0].summary+' It is currently '+body.currently.temperature+' degree out. There is a '+body.currently.precipProbability+'% chance of rain.'))
         }
 
     })
